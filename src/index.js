4218750c01019bcde8bc8e0a830d061e13195705
amngupta <amangupta96@outlook.com>
2017-03-19 02:31:47 -0700

    More endpoints v2.0

--- src/index.js
+++ src/index.js
@@ -40,7 +40,16 @@ server.get('/subscription/:subscriptionId', function (req, res, next) {
 
 });
 
-console.log('hello world')
+server.get('/subscriptionType/:subscriptionTypeId', function (req, res, next) {
+  console.log(req.params.subscriptionTypeId);
+  res.send(req.params);
+  return next();
+});
 
-
-//var columns = ['id'];
+server.post('/subscriptionType/create', function (req, res, next) {
+  if (req.body) {
+    console.log(req.body);
+    res.send(req.params);
+    return next();
+  }
+});
