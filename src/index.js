5e43a1f8b2b8059eb8e62d38731d5f53c5277c36
Tanya <tanyi3025@gmail.com>
2017-03-19 04:46:39 -0700

    first api success

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 var pgp = require('pg-promise')();
 var cn = {
-    host: 'kfr-78d8gx42.cloudapp.net',
-    port: 26257,
-    database: 'bank',
-    user: 'root'
+  host: 'kfr-78d8gx42.cloudapp.net',
+  port: 26257,
+  database: 'bank',
+  user: 'root'
 };
 var db = pgp(cn);
 
@@ -30,12 +30,12 @@ server.get('/subscription/:subscriptionId', function (req, res, next) {
     console.log(req.params.subscriptionId);
     res.send(response);
 
-      return next();
+    return next();
   })
   .catch((err) => {
     res.send(err);
     console.log(err)
-      return next();
+    return next();
   });
 
 });
@@ -48,8 +48,37 @@ server.get('/subscriptionType/:subscriptionTypeId', function (req, res, next) {
 
 server.post('/subscriptionType/create', function (req, res, next) {
   if (req.body) {
-    console.log(req.body);
-    res.send(req.params);
-    return next();
-  }
+    //body: name, cost, business_id, billing_type, create_date
+    // let name = req.params.name;
+    // let cost = req.params.cost;
+    // let businessId = req.params.bussinessId;
+    // let billingType = req.params.billingType;
+    // let createDate = moment().format('YYYY-MM-DD');
+
+    let name = 'bitch';
+    let cost = '100';
+    let businessId = '101';
+    let billingType = 'years';
+    let createDate = "2017-02-09";
+
+    return db.query('select * from bank.businesses where id = '+businessId+';')
+    .then((response)=>{
+      console.log(response);
+      return db.query('insert into bank.subscriptions_type (business_id,name,cost,create_date,billing_type) values (' + businessId+',\''+ name+'\','+cost+',\''+ createDate +'\',\''+ billingType + '\');')
+    })
+  .then((response)=>{
+    console.log(response,"here");
+  return db.query('SELECT * FROM bank.subscriptions_type where business_id= '+ businessId +' and name = \''+ name +'\' and cost = '+cost+' and billing_type = \''+billingType+'\';')
+})
+.then((response)=>{
+  console.log(response)
+  res.send(response);
+  return next();
+})
+.catch((err) => {
+  res.send(err);
+  console.log(err)
+  return next();
+});
+}
 });
